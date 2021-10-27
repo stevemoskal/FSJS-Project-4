addd0e130379b56453d91444ddc550dc12bf316c
stevemoskal <stevemoskal@gmail.com>
2021-10-27 19:33:53 -0400

    added classes, methods

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,38 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+   constructor(){
+     this.missed = 0;
+     this.phrases = this.createPhrases();
+     this.activePhrase = this.getRandomPhrase();
+   }
+
+   /* Creates phrases for use in game
+   * @return {array} An array of phrases that could be used in the game
+   */
+
+   createPhrases() {
+     const phrases =  ['American Staffordshire Terrier',
+            'Alaskan Malamute',
+            'Bernese Mountain Dog',
+            'Cavalier King Charles Spaniel',
+            'English Cocker Spaniel',
+            'French Bulldog',
+            'Labrador Retriever',
+            'Poodle',
+            'Puggle'];
+    return phrases;
+   }
+
+   /**
+   * Selects random phrase from phrases property
+   * @return {Object} Phrase object chosen to be used
+   */
+
+   getRandomPhrase() {
+     let phraseIndex = Math.floor(Math.random() * this.phrases.length);
+     return this.phrases[phraseIndex];
+   }
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,16 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+   constructor(phrase){
+     this.phrase = phrase.toLowerCase();
+   }
+
+   addPhraseToDisplay() {
+     let phrase = this.phrase;
+     for (let i = 0; i < phrase.length; i++) {
+
+     }
+   }
+ }
